818952a95552e5266e5254bff25e699710825620
Jay Tolentino <jay@nuzzel.com>
2014-10-29 12:27:02 -0700

    recovered lost compose tweet icon in menu LOL

--- app/src/main/java/com/codepath/apps/birdfeed/activities/FeedActivity.java
+++ app/src/main/java/com/codepath/apps/birdfeed/activities/FeedActivity.java
@@ -4,6 +4,7 @@ package com.codepath.apps.birdfeed.activities;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import com.codepath.apps.birdfeed.R;
 import com.codepath.apps.birdfeed.fragments.ComposeTweetFragment;
@@ -15,6 +16,21 @@ public class FeedActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.feed, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onComposeTweet(MenuItem item) {
